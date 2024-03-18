a41ae41330780ae551684dd2588fd22963a0b39c
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-03-18 14:11:34 +0100

    update grille sans transfert de diff

--- main.py
+++ main.py
@@ -1,21 +1,71 @@
 import tkinter as tk
 import tkinter.font as tkFont
+import tkinter.messagebox
 
+# DEMANDER POUR RECUPERER LA VARIABLE STR
 
-root = tk.Tk()
+class Difficulty():
+    
+    def __init__(self):
+        self.x = 0
+        self.y = 0
 
-font = tkFont.Font(family="Helvetica", size=20, weight = tkFont.BOLD)
-blank_image = tk.PhotoImage()
+    def update_menubutton_text(self, *args):
+        self.difficulty = self.diff_selection
+        self.diff_button.config(text=f"Difficulté : {self.difficulty}")
 
-for i in range(6):
-    for j in range(15):
-        b = tk.Button(root, image=blank_image,
-                           font=font, compound=tk.CENTER)
+    def difficulty_choice(self):
+        
+        self.root1 = tk.Tk()
+        self.root1.geometry("200x200")
+        self.label = tk.Label(self.root1, text="Choix de la difficulté : ")
+        self.label.pack()
+        self.diff_selection = tk.StringVar()
+        self.diff_button = tk.Menubutton(self.root1, text="Difficulté ?")
+        self.menu = tk.Menu(self.diff_button, tearoff=False)
+        self.diff_button["menu"] = self.menu
+        self.menu.add_command(label="Facile" , command=lambda: self.diff_selection.set("Facile"))
+        self.menu.add_command(label="Moyen" , command=lambda: self.diff_selection.set("Moyen"))
+        self.menu.add_command(label="Difficile" , command=lambda: self.diff_selection.set("Difficile"))
+        self.diff_button.pack()
+        self.diff_selection = tk.StringVar()
+        self.diff_selection.trace_add("write", self.update_menubutton_text)
+        btn = tk.Button(self.root1, text="Confirmer", command=self.root1.destroy)
+        btn.pack()
+        self.root1.mainloop()
+        
+        # tk.messagebox.showinfo("Choix de la difficulté : ")
+        return self.difficulty
 
-    # get the height of the font to use as the square size
-        square_size = font.metrics('linespace')
-        b.config(width=square_size, height=square_size)
+    def creation_grid(self, a):
+        self.root = tk.Tk()
+        self.font = tkFont.Font(family="Helvetica", size=20, weight = tkFont.BOLD)
+        self.blank_image = tk.PhotoImage()
+        
+        self.x=5
+        self.y=5
+        if a == "Facile":
+            self.x = 10
+            self.y = 5
+        elif a == "Moyen":
+            self.x = 10
+            self.y = 10
+        elif a == "Difficile":
+            self.x = 15
+            self.y = 10
 
-        b.grid(row = i, column = j, sticky = "NWSE")
+        for i in range(self.y):
+            for j in range(self.x):
+                b = tk.Button(self.root, image=self.blank_image,
+                                font=self.font, compound=tk.CENTER)
 
-root.mainloop()
+            # get the height of the font to use as the square size
+                square_size = self.font.metrics('linespace')
+                b.config(width=square_size, height=square_size)
+
+                b.grid(row = i, column = j, sticky = "NWSE")
+
+        self.root.mainloop()
+        
+test = Difficulty()
+test.creation_grid(test.difficulty_choice())
