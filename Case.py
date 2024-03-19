8e1d94250d03c0506b3acdc28dbe9ad890e4d1f1
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-03-19 14:57:01 +0100

    attente images

--- Case.py
+++ Case.py
@@ -41,20 +41,18 @@ class Case():
         # Ça ferme la fenêtre et revient au choix de la difficulté
         pass
     
-    def plant_bombs(self):                  # Sous-programme qui plante les bombes au hasard
+    def plant_bombs(self):                  # Sous-programme qui plante les bombes au hasard et compte les cases
         for i in range(self.y):
             for j in range(self.x):
                 a = random.randint(0,100)
                 if a <= 15:
                     print("BOMBE")
                     self.dict_cases[(i,j)] = 'X'
-                    # self.tab_valeur_case[i][j] = 1
-                    # self.cpt_cases_mined += 1
+                    self.cpt_cases_mined += 1
                 elif a > 15:
                     print("PAS DE BOMBE")
                     self.dict_cases[(i,j)] = '.'
-                    # self.tab_valeur_case[i][j] = 0
-                    # self.cpt_cases_demined += 1
+                    self.cpt_cases_demined += 1
             print(self.dict_cases[(i,j)])
     
     def question_mark(self):                # Sous-programme qui affiche '?' ou rien si déjà '?'
@@ -74,6 +72,62 @@ class Case():
     
     def set_question(self, a):
         self.question_mark_var = a
+        
+    def bomb_or_not(self, co_x, co_y):      # Sous-programme qui permet de compter les bombes autour
+        if self.dict_cases[(co_x,co_y)] == 'X':
+            return 1
+        else:
+            return 0
     
-    def verification_case(self, co_x, co_y):
-        pass
+    def verification_case(self, co_x, co_y):        # Sous-programme qui vérifie et compte les cases bombes autour avant de changer la case
+        cpt = 0
+        if co_x == 0 and co_y == 0:                             # Coin haut gauche
+            cpt = self.bomb_or_not(co_x +1,co_y) + self.bomb_or_not(co_x +1,co_y +1) + self.bomb_or_not(co_x,co_y +1)
+        elif co_x == (self.x -1) and co_y == 0:                 # Coin haut droit
+            cpt = self.bomb_or_not(co_x -1,co_y) + self.bomb_or_not(co_x -1,co_y +1) + self.bomb_or_not(co_x,co_y +1)
+        elif co_x == 0 and co_y == (self.y -1):                 # Coin bas gauche
+            cpt = self.bomb_or_not(co_x +1,co_y) + self.bomb_or_not(co_x +1,co_y -1) + self.bomb_or_not(co_x,co_y -1)
+        elif co_x == (self.x -1) and co_y == (self.y -1):       # Coin bas droit
+            cpt = self.bomb_or_not(co_x -1,co_y) + self.bomb_or_not(co_x -1,co_y -1) + self.bomb_or_not(co_x,co_y -1)
+        elif co_x == 0:                                         # Côté gauche de la grille
+            cpt = self.bomb_or_not(co_x,co_y -1) + self.bomb_or_not(co_x +1,co_y -1) + self.bomb_or_not(co_x +1,co_y)
+            + self.bomb_or_not(co_x +1,co_y +1) + self.bomb_or_not(co_x,co_y +1)
+        elif co_y == 0:                                         # Côté haut de la grille
+            cpt = self.bomb_or_not(co_x -1,co_y) + self.bomb_or_not(co_x -1,co_y +1) + self.bomb_or_not(co_x,co_y +1)
+            + self.bomb_or_not(co_x +1,co_y +1) + self.bomb_or_not(co_x +1,co_y)
+        elif co_x == (self.x -1):                               # Côté droit de la grille
+            cpt = self.bomb_or_not(co_x,co_y -1) + self.bomb_or_not(co_x -1,co_y -1) + self.bomb_or_not(co_x -1,co_y)
+            + self.bomb_or_not(co_x -1,co_y +1) + self.bomb_or_not(co_x,co_y +1)
+        elif co_y == (self.y -1):                               # Côté bas de la grille
+            cpt = self.bomb_or_not(co_x -1,co_y) + self.bomb_or_not(co_x -1,co_y -1) + self.bomb_or_not(co_x,co_y -1)
+            + self.bomb_or_not(co_x +1,co_y -1) + self.bomb_or_not(co_x +1,co_y)
+        else:                                                   # Case random au milieu
+            cpt = self.bomb_or_not(co_x -1,co_y) + self.bomb_or_not(co_x -1,co_y -1) + self.bomb_or_not(co_x,co_y -1) + self.bomb_or_not(co_x +1,co_y -1)
+            + self.bomb_or_not(co_x +1,co_y) + self.bomb_or_not(co_x +1,co_y +1) + self.bomb_or_not(co_x,co_y +1) + self.bomb_or_not(co_x -1,co_y +1)
+        
+        if cpt == 0:                                # Si 0 bombe autour, check les cases autour
+            self.dict_cases[(co_x,co_y)] = '0'
+            self.check_bomb(co_x -1,co_y)
+            self.check_bomb(co_x -1,co_y -1)
+            self.check_bomb(co_x,co_y -1)
+            self.check_bomb(co_x +1,co_y -1)
+            self.check_bomb(co_x +1,co_y)
+            self.check_bomb(co_x +1,co_y +1)
+            self.check_bomb(co_x,co_y +1)
+            self.check_bomb(co_x -1,co_y +1)
+        elif cpt == 1:                              # Si bombe alors affiche le nombre et stop
+            self.dict_cases[(co_x,co_y)] = '1'
+        elif cpt == 2:
+            self.dict_cases[(co_x,co_y)] = '2'
+        elif cpt == 3:
+            self.dict_cases[(co_x,co_y)] = '3'
+        elif cpt == 4:
+            self.dict_cases[(co_x,co_y)] = '4'
+        elif cpt == 5:
+            self.dict_cases[(co_x,co_y)] = '5'
+        elif cpt == 6:
+            self.dict_cases[(co_x,co_y)] = '6'
+        elif cpt == 7:
+            self.dict_cases[(co_x,co_y)] = '7'
+        elif cpt == 8:
+            self.dict_cases[(co_x,co_y)] = '8'           
