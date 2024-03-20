a4ab129a207566a9a51507ebbc885c2d1cc56885
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-03-20 09:14:48 +0100

    Flag, question mark

--- GUI.py
+++ GUI.py
@@ -22,9 +22,9 @@ pygame.display.set_caption("MINESWEEPER")
 clock = pygame.time.Clock()
 running = True
 
-# load image and scale it to the correct size
-button_image = pygame.image.load("button.png")
-button_image = pygame.transform.scale(button_image, (WIDTH, HEIGHT))
+# load images and scale them to the correct size
+image_button = pygame.image.load("button.png")
+image_button = pygame.transform.scale(image_button, (WIDTH, HEIGHT))
 
 image1 = pygame.image.load("1.png")
 image1 = pygame.transform.scale(image1, (WIDTH, HEIGHT))
@@ -32,8 +32,11 @@ image1 = pygame.transform.scale(image1, (WIDTH, HEIGHT))
 image_flag = pygame.image.load("flag.png")
 image_flag = pygame.transform.scale(image_flag, (WIDTH, HEIGHT))
 
-# Create a matrix to store flag states, 2D list with FALSE values
-flags = [[False] * 9 for _ in range(9)]
+image_question_mark = pygame.image.load("question_mark.png")
+image_question_mark = pygame.transform.scale(image_question_mark, (WIDTH, HEIGHT))
+
+# Create a matrix to store flag states
+flags = [["none"] * 9 for _ in range(9)]
 
 # Main
 while running:
@@ -50,25 +53,32 @@ while running:
 
                 # considers the click only inside the grid
                 grid[row][column] = 1
-                print("Left Click ", pos, "Grid coordinates: ", row, column)
+                #print("Left Click ", pos, "Grid coordinates: ", row, column)
             elif event.button == 3:  # Right click
                 pos = pygame.mouse.get_pos()
                 column = pos[0] // (WIDTH + MARGIN)
                 row = pos[1] // (HEIGHT + MARGIN)
                 print("Right click", pos, "Grid coordinates", row, column)
 
-                # Toggle flag state
-                flags[row][column] = not flags[row][column]
+                # Toggle flag state to know the status of each grid position
+                if flags[row][column] == "none":
+                    flags[row][column] = "flag"
+                elif flags[row][column] == "flag":
+                    flags[row][column] = "question"
+                else:
+                    flags[row][column] = "none"
 
     screen.fill("grey")
 
     # create grid
     for row in range(9):
         for column in range(9):
-            if flags[row][column]:      #verify if flag is in position row column
+            if flags[row][column] == "flag":
                 screen.blit(image_flag, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
+            elif flags[row][column] == "question":
+                screen.blit(image_question_mark, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
             else:
-                screen.blit(button_image, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
+                screen.blit(image_button, ((MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN))
 
     pygame.display.update()
     clock.tick(60)  # limit to 60 FPS
