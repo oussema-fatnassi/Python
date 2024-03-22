259243de9b4aeb80c2ee4dfa075528c3de63790c
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-03-22 20:56:21 +0100

    Fix bug du timer. Timer se refresh correctement

--- Main_test.py
+++ Main_test.py
@@ -2,14 +2,14 @@ from GUI_test import GUI
 from Case import Case
 from Difficulty import Difficulty
 import sys
+import time
 
 import random
 import pygame
 from pygame.locals import *
 
 
-def main(GUI , chosen_difficulty):
-    #Difficulty_instance = Difficulty()
+def main(GUI, chosen_difficulty):
     case_instance = Case()
     GUI.init_grille(chosen_difficulty)
     timer_started = False
@@ -21,37 +21,37 @@ def main(GUI , chosen_difficulty):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 GUI.running = False
+                pygame.quit()
+                sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:           # Left click
-                    pos = pygame.mouse.get_pos()                                                              # Récupère la position du clique
-                    if GUI.x >= 0 and GUI.x < GUI.x_max and GUI.y >= 0 and GUI.y < GUI.y_max:                 # Check position mouse dans la grille
-                        print("Left Click ", pos, "Matrice_case coordinates: ", GUI.x, GUI.y)
-                        if not timer_started:
-                            start_timer = pygame.time.get_ticks()
-                            timer_started = True         
-                        #GUI.pos_case()
-                        print(GUI.x_max, GUI.y_max)
-                        
+                if event.button == 1 and not timer_started:                                     # Left click and timer not started
+                    if GUI.x >= 0 and GUI.x < GUI.x_max and GUI.y >= 0 and GUI.y < GUI.y_max:
                         case_instance.clique()
-                        #GUI_test.cpt_cases_mined(case_instance.number_of_bombs)
+                        GUI.running = True
+                        start_timer = pygame.time.get_ticks()
+                        timer_started = True                                                    # Set timer_started to True
                     else:
                         break
-                    
-                elif event.button == 3:         # Right click
-                    pos = pygame.mouse.get_pos()
-                    
+                elif event.button == 1 and timer_started:                                       # Left click and timer started
                     if GUI.x >= 0 and GUI.x < GUI.x_max and GUI.y >= 0 and GUI.y < GUI.y_max:
-                        print("Right click", pos, "Matrice_case coordinates", GUI.x, GUI.y)
+                        case_instance.clique()
+                    else:
+                        break
+                elif event.button == 3:  # Right click
+                    if GUI.x >= 0 and GUI.x < GUI.x_max and GUI.y >= 0 and GUI.y < GUI.y_max:
                         case_instance.flag()
                     else:
                         break
-    
-            if timer_started:
-                        elapsed_time = pygame.time.get_ticks() - start_timer
-                        GUI_test.timer_gui(elapsed_time)
+                                                                                            
+        timer_rect = pygame.Rect((GUI.screen_width - 85, GUI.screen_height - 55, 85, 50))     # Clear only the portion of the screen where the timer is displayed
+        GUI.screen.fill((0, 0, 0), timer_rect)
 
-
-    pygame.display.flip()
+        if timer_started:                                                                       # Only update timer if it has started
+            passed_time = pygame.time.get_ticks() - start_timer
+            GUI.timer_gui(passed_time)
+        else:
+            GUI.timer_gui(0)                                                                    # Display "000" before the first click
+        pygame.display.flip()
     pygame.quit()
 
 Difficulty_instance = Difficulty()
