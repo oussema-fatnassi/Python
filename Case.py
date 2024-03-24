941748668e2f2435b148493da4d6b148289c0ac9
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-03-24 18:39:36 +0100

    Case complet avec print de test

--- Case.py
+++ Case.py
@@ -1,45 +1,151 @@
 import random
 import pygame
 from pygame.locals import *
-from GUI import GUI
+from GUI_test import GUI
+from Difficulty import Difficulty
 
 class Case(GUI):
     
-    def __init__(self):
+    def __init__(self, chosen_difficulty):                                          # Receive chosen difficulty level
         super().__init__()
         self.load_images()
         self.flag_var = 0
         self.question_mark_var = 0
+        self.chosen_difficulty = chosen_difficulty                                  # Store chosen difficulty level
+        self.init_grid()                                                            # Initialize grid based on difficulty level
     
-    def check_bomb(self, co_x, co_y):                       # Function that checks if the case is a bomb or not
+    def init_grid(self):
+        if self.chosen_difficulty == "easy":                                        # Initialize other variables for easy difficulty
+            self.x_max = 9
+            self.y_max = 9
+            self.screen_width = 270
+            self.screen_height = 330
+        elif self.chosen_difficulty == "medium":                                    # Initialize other variables for medium difficulty
+            self.x_max = 16
+            self.y_max = 16
+            self.screen_width = 480
+            self.screen_height = 540
+        elif self.chosen_difficulty == "hard":                                      # Initialize other variables for hard difficulty
+            self.x_max = 30
+            self.y_max = 16
+            self.screen_width = 900
+            self.screen_height = 540
+        else:                                                                       # Initialize other variables for default difficulty                
+            self.x_max = 9
+            self.y_max = 9
+            self.screen_width = 270
+            self.screen_height = 330
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))  # Adjust screen size
+        self.Matrice_case = []                                                          # Initialisation of the grid
+        for row in range(self.y_max): 
+            self.Matrice_case.append([])
+            for col in range(self.x_max): 
+                self.Matrice_case[row].append('')
+    
+    def check_bomb(self, co_x, co_y):                                                   # Function that checks if the case is a bomb or not
         if (co_x >= 0 and co_x < self.x_max) and (co_y >= 0 and co_y < self.y_max):
-            if self.Matrice_case[co_y][co_x] == 'X':        # If it is a bomb then game over
+            if self.Matrice_case[co_y][co_x] == 'X':                                    # If it is a bomb then game over
                 self.Matrice_case[co_y][co_x] = 'x'
                 self.game_over()
             else:
-                if self.Matrice_case[co_y][co_x] != '0':    # If the case has any value other than 0 it will show the image of that value
+                if self.Matrice_case[co_y][co_x] != '0':                                # If the case has any value other than 0 it will show the image of that value
                     self.show(co_x,co_y)
                     self.Matrice_case[co_y][co_x] = '9'
                 else:
-                    self.recursivity(co_x,co_y)             # If the value is 0 then it launches the function recursivity
+                    self.recursivity(co_x,co_y)                                         # If the value is 0 then it launches the function recursivity
 
+    def check_victory(self):                                                            # Function that checks if the player has won
+        cpt_cases = 0
+        for i in range(self.y_max):
+            for j in range(self.x_max):
+                if self.Matrice_case[i][j] == '9':
+                    cpt_cases += 1
+        print(cpt_cases, " / ", self.cpt_cases_demined)
+        if cpt_cases == self.cpt_cases_demined:                                         # If the number of cases discovered is equal to the number of cases without bomb then the player wins
+            self.game_win()
+    
     def clique(self):                               # Function for each left click
         self.pos_case()
-        if self.cpt_cases_mined == 0:       # Case for the first click
+        if self.cpt_cases_mined == 0:               # Case for the first click
             self.plant_bombs(self.y_max,self.x_max)
-        else:                               # Case for others clicks
+        else:                                       # Case for others clicks
+            self.check_victory()
             self.check_bomb(self.x,self.y)
     
-    def flag(self):                                 # Function to check if there is a flag/question mark or not and then show it
+    def flag(self):
         self.pos_case()
-        if self.Matrice_case[self.y][self.x] == ('0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or 'X'):
-            if self.flag_var == 0:
-                self.screen.blit(self.image_flag, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
+        # Check if the case already has a flag or question mark
+        if self.Matrice_case[self.y][self.x] in ['F0','F1','F2','F3','F4','F5','F6','F7','F8','FX','?0','?1','?2','?3','?4','?5','?6','?7','?8','?X']:
+            if self.Matrice_case[self.y][self.x] in ['F0','F1','F2','F3','F4','F5','F6','F7','F8','FX']:  # If it's a flag, change to question mark
+                if self.Matrice_case[self.y][self.x] == 'F0':
+                    self.Matrice_case[self.y][self.x] = '?0'
+                elif self.Matrice_case[self.y][self.x] == 'F1':
+                    self.Matrice_case[self.y][self.x] = '?1'
+                elif self.Matrice_case[self.y][self.x] == 'F2':
+                    self.Matrice_case[self.y][self.x] = '?2'
+                elif self.Matrice_case[self.y][self.x] == 'F3':
+                    self.Matrice_case[self.y][self.x] = '?3'
+                elif self.Matrice_case[self.y][self.x] == 'F4':
+                    self.Matrice_case[self.y][self.x] = '?4'
+                elif self.Matrice_case[self.y][self.x] == 'F5':
+                    self.Matrice_case[self.y][self.x] = '?5'
+                elif self.Matrice_case[self.y][self.x] == 'F6':
+                    self.Matrice_case[self.y][self.x] = '?6'
+                elif self.Matrice_case[self.y][self.x] == 'F7':
+                    self.Matrice_case[self.y][self.x] = '?7'
+                elif self.Matrice_case[self.y][self.x] == 'F8':
+                    self.Matrice_case[self.y][self.x] = '?8'
+                elif self.Matrice_case[self.y][self.x] == 'FX':
+                    self.Matrice_case[self.y][self.x] = '?X'
+                self.screen.blit(self.image_question_mark, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
                 pygame.display.update()
-                self.flag_var = 1
-            elif self.flag_var == 1:
-                self.question_mark()
-    
+            else:  # If it's a question mark, change to button
+                if self.Matrice_case[self.y][self.x] == '?0':
+                    self.Matrice_case[self.y][self.x] = '0'
+                elif self.Matrice_case[self.y][self.x] == '?1':
+                    self.Matrice_case[self.y][self.x] = '1'
+                elif self.Matrice_case[self.y][self.x] == '?2':
+                    self.Matrice_case[self.y][self.x] = '2'
+                elif self.Matrice_case[self.y][self.x] == '?3':
+                    self.Matrice_case[self.y][self.x] = '3'
+                elif self.Matrice_case[self.y][self.x] == '?4':
+                    self.Matrice_case[self.y][self.x] = '4'
+                elif self.Matrice_case[self.y][self.x] == '?5':
+                    self.Matrice_case[self.y][self.x] = '5'
+                elif self.Matrice_case[self.y][self.x] == '?6':
+                    self.Matrice_case[self.y][self.x] = '6'
+                elif self.Matrice_case[self.y][self.x] == '?7':
+                    self.Matrice_case[self.y][self.x] = '7'
+                elif self.Matrice_case[self.y][self.x] == '?8':
+                    self.Matrice_case[self.y][self.x] = '8'
+                elif self.Matrice_case[self.y][self.x] == '?X':
+                    self.Matrice_case[self.y][self.x] = 'X'
+                self.screen.blit(self.image_button, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
+                pygame.display.update()
+        elif self.Matrice_case[self.y][self.x] in ['0','1','2','3','4','5','6','7','8','X']:  # If the case has no flag or question mark
+            if self.Matrice_case[self.y][self.x] == '0':
+                self.Matrice_case[self.y][self.x] = 'F0'
+            elif self.Matrice_case[self.y][self.x] == '1':
+                self.Matrice_case[self.y][self.x] = 'F1'
+            elif self.Matrice_case[self.y][self.x] == '2':
+                self.Matrice_case[self.y][self.x] = 'F2'
+            elif self.Matrice_case[self.y][self.x] == '3':
+                self.Matrice_case[self.y][self.x] = 'F3'
+            elif self.Matrice_case[self.y][self.x] == '4':
+                self.Matrice_case[self.y][self.x] = 'F4'
+            elif self.Matrice_case[self.y][self.x] == '5':
+                self.Matrice_case[self.y][self.x] = 'F5'
+            elif self.Matrice_case[self.y][self.x] == '6':
+                self.Matrice_case[self.y][self.x] = 'F6'
+            elif self.Matrice_case[self.y][self.x] == '7':
+                self.Matrice_case[self.y][self.x] = 'F7'
+            elif self.Matrice_case[self.y][self.x] == '8':
+                self.Matrice_case[self.y][self.x] = 'F8'
+            elif self.Matrice_case[self.y][self.x] == 'X':
+                self.Matrice_case[self.y][self.x] = 'FX'
+            self.screen.blit(self.image_flag, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
+            pygame.display.update()
+
     def game_over(self):                            # Function in case of defeat
         self.screen.blit(self.image_final_bomb, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
         pygame.display.update()
@@ -51,8 +157,7 @@ class Case(GUI):
 
     def game_win(self):                             # Function in case of victory
         print("Test victoire")
-        # Ça finit le jeu avec un message de victoire
-        # Ça ferme la fenêtre et revient au choix de la difficulté
+        
     
     def plant_bombs(self,y,x):                      # Function to plant the bombs at the beginning
         self.pos_case()
@@ -81,19 +186,6 @@ class Case(GUI):
                     self.Matrice_case[i][j] = str(cpt)
             print(self.Matrice_case[i])             # Cheating print to have the solution
         self.check_bomb(self.x,self.y)              # Check the first case
-    
-    def question_mark(self):                        # Function to show a question mark if there is already a flag
-        self.pos_case()
-        if self.Matrice_case[self.y][self.x] == '.' or self.Matrice_case[self.y][self.x] == 'X':
-            if self.question_mark_var == 0:
-                self.screen.blit(self.image_question_mark, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
-                pygame.display.update()
-                self.question_mark_var = 1
-            elif self.question_mark_var == 1:
-                self.screen.blit(self.image_button, ((self.MARGIN + self.WIDTH) * self.x + self.MARGIN, (self.MARGIN + self.HEIGHT) * self.y + self.MARGIN))
-                pygame.display.update()
-                self.flag_var = 0
-                self.question_mark_var = 0
              
     def recursivity(self,co_x,co_y):                # Function for recursivity
         self.show(co_x,co_y)
@@ -106,40 +198,31 @@ class Case(GUI):
                         if self.Matrice_case[co_y][co_x] == '0':
                             self.Matrice_case[co_y][co_x] = '9'
                         self.recursivity(neighbor_x,neighbor_y)
+        self.check_victory()
                         
     def show(self,x,y):                             # Function to show an image corresponding to the value of the chosen case
         a = self.Matrice_case[y][x]
         if a == '0':
             self.screen.blit(self.image_empty, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '1':
             self.screen.blit(self.image1, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '2':
             self.screen.blit(self.image2, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '3':
             self.screen.blit(self.image3, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '4':
             self.screen.blit(self.image4, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '5':
             self.screen.blit(self.image5, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '6':
             self.screen.blit(self.image6, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '7':
             self.screen.blit(self.image7, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
-            self.cpt_cases_demined -= 1
         elif a == '8':
             self.screen.blit(self.image8, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
         elif a == 'X':
             self.screen.blit(self.image_bomb, ((self.MARGIN + self.WIDTH) * x + self.MARGIN , (self.MARGIN + self.HEIGHT) * y + self.MARGIN ))
         elif a == '9':
-            self.cpt_cases_demined += 1
+            pass
+        self.Matrice_case[y][x] = '9'
         pygame.display.update()                     # Update the gird for the visual
-        print(self.cpt_cases_demined)
-        if self.cpt_cases_demined == 0:
-            self.game_win()
